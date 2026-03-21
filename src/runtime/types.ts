a9182164e55129c7790762cf8cab795f874aea56
Samyra312007 <samayra312007@gmail.com>
2026-03-21 18:28:02 +0530

    Add runtime types definition

---
+++ src/runtime/types.ts
@@ -0,0 +1,42 @@
+export enum RuntimeType {
+  Undefined = 'Undefined',
+  Null = 'Null',
+  Boolean = 'Boolean',
+  Number = 'Number',
+  String = 'String',
+  Object = 'Object',
+  Array = 'Array',
+  Function = 'Function',
+  RegExp = 'RegExp',
+  Map = 'Map',
+  Set = 'Set',
+  WeakMap = 'WeakMap',
+  WeakSet = 'WeakSet'
+}
+
+export interface RuntimeValue {
+  type: RuntimeType;
+  value: any;
+}
+
+export class JSValue implements RuntimeValue {
+  type: RuntimeType;
+  value: any;
+  
+  constructor(type: RuntimeType, value: any) {
+    this.type = type;
+    this.value = value;
+  }
+  
+  toString(): string {
+    return String(this.value);
+  }
+  
+  toNumber(): number {
+    return Number(this.value);
+  }
+  
+  toBoolean(): boolean {
+    return Boolean(this.value);
+  }
+}
